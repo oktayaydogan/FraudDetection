ba22afc8bd22ebaa41532cba63d1e150da473d5c
Oktay Aydoğan <31257020+oktayaydogan@users.noreply.github.com>
2025-01-08 16:24:28 +0300

    Enhance client.pl to include section dividers for improved readability of user transaction status outputs

--- client.pl
+++ client.pl
@@ -12,30 +12,42 @@ sorgula :-
     islem_sikligi:islem_sayisi(kullanici1, 0, 24, Sayi),
     writeln(Sayi),
 
+    writeln('-----------------------------------'),
+
     writeln('2. Kullanıcının şüpheli işlem durumu:'),
     (islem_sikligi:supheli_islem(kullanici1, 0, 24) ->
         writeln('Şüpheli işlem var!');
         writeln('Şüpheli işlem yok.')),
+
+    writeln('-----------------------------------'),
 
     writeln('3. Anormal işlem durumu:'),
     (islem_miktari:anormal_islem(kullanici1, 5000) ->
         writeln('Anormal işlem tespit edildi!');
         writeln('Anormal işlem yok.')),
 
+    writeln('-----------------------------------'),
+
     writeln('4. Konum uyuşmazlığı kontrolü:'),
     (islem_konumu:konum_uyusmazligi(kullanici1) ->
         writeln('Konum uyuşmazlığı var!');
         writeln('Konum uyuşmazlığı yok.')),
+
+    writeln('-----------------------------------'),
 
     writeln('5. Farklı konumlarda kısa süreli işlem risk skoru:'),
     farkli_konum:farkli_konum_risk(kullanici1, Risk),
     format('Toplam risk puanı: ~w~n', [Risk]),
     (Risk > 10 -> writeln('Risk eşik değeri aşıldı: Şüpheli işlem!'); writeln('Risk eşik değeri aşılmadı: Normal işlem.')),
 
+    writeln('-----------------------------------'),
+
     writeln('6. Yeni cihaz tespiti:'),
     (yeni_cihaz:yeni_cihaz_tespiti(kullanici1) ->
         writeln('Yeni cihazdan işlem tespit edildi!');
         writeln('Yeni cihaz kullanılmadı.')),
+
+    writeln('-----------------------------------'),
 
     writeln('7. Kullanıcı davranış sapması kontrolü:'),
     (davranis_analizi:davranis_sapmasi(kullanici1) ->
