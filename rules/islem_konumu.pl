ce4bcf31535516f0fed30f1ec3e95f886789afee
Oktay Aydoğan <31257020+oktayaydogan@users.noreply.github.com>
2025-01-08 16:24:22 +0300

    Enhance islem_konumu module to log user location history and count of unique locations for better tracking of location discrepancies

--- rules/islem_konumu.pl
+++ rules/islem_konumu.pl
@@ -1,13 +1,15 @@
 :- module(islem_konumu, [konum_uyusmazligi/1]).
-:- use_module('../data/islem_verileri'). % Veriler dahil ediliyor
+:- use_module('../data/islem_verileri').
 
 % Kullanıcının önceki işlemlerindeki konumları listeleme
 kullanici_konumlari(Kullanici, Konumlar) :-
-    findall(Konum, islem(Kullanici, _, _, Konum, _, _), Konumlar).
+    findall(Konum, islem(Kullanici, _, _, Konum, _, _), Konumlar),
+    writeln(['Kullanıcı konumları:', Kullanici, '=>', Konumlar]).
 
 % Konum uyuşmazlığını kontrol eden kural
 konum_uyusmazligi(Kullanici) :-
     kullanici_konumlari(Kullanici, Konumlar),
-    list_to_set(Konumlar, UnikKonumlar), % Konumların eşsiz listesini oluştur
+    list_to_set(Konumlar, UnikKonumlar),
     length(UnikKonumlar, Say),
-    Say > 1. % Birden fazla farklı konum varsa uyuşmazlık vardır
+    writeln(['Farklı konum sayısı:', Kullanici, '=>', Say]),
+    Say > 1.
